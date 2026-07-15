4285e9e56f997307706702987726ba1a637e3317
Bhavyasrireddy12 <it1273bhavyasrireddy@gmail.com>
2026-07-15 19:24:14 +0530

    Time: 38 ms (22.39%), Space: 19.2 MB (85.77%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -4,9 +4,9 @@ class Solution:
         evensum=0
         for i in range(1,n):
             if i%2==0:
-                2*i
+               evensum+=2*i
             else:
-                2*i-1
+                oddsum+=2*i-1
             gcd(oddsum,evensum)    
         return n
         
