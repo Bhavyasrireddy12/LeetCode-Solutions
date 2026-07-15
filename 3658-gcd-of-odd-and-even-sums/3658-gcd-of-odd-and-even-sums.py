ca2e3c1771069b80fce447cee2d6effcc90e2ec4
Bhavyasrireddy12 <it1273bhavyasrireddy@gmail.com>
2026-07-15 19:26:19 +0530

    Time: 30 ms (31.24%), Space: 19.3 MB (53.03%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -1,12 +1,11 @@
+from math import gcd
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
         oddsum=0
         evensum=0
-        for i in range(1,n):
-            if i%2==0:
+        for i in range(1,n+1):
                evensum+=2*i
-            else:
-                oddsum+=2*i-1
-            gcd(oddsum,evensum)    
-        return n
+               oddsum+=2*i-1
+        return gcd(oddsum,evensum)    
+        
         
